b438dda057b302dc65be28e1639b53ddf13943d4
Nycticoraci <37623397+Nycticoraci@users.noreply.github.com>
2020-10-24 02:40:59 -0400

    Update partA.c

--- assignmentOne/partA.c
+++ assignmentOne/partA.c
@@ -7,25 +7,21 @@
 #include<sys/wait.h> //for wait call
 #include<unistd.h> //for fork call
 #include<stdlib.h> //general utilities
-// Part A: Working with processes in Linux
-
-
-//Part 1a: write a program prog that repeatedly creates a sub-process using fork, and waits until it terminates. 
-//Each child process prints the child PID (using getpid and exit)
-//
-//Part 1b: Modify prog so that the child process spawns another program b using execve. Program b immediately exits.
 
 int main(int argc, char* argv[])
 {
+	//convert user input to integer
 	int n = atoi(argv[1]);
 	//printf("user argument is: %d\n", n);
-	
+
+	//set parameters for user input range
 	if (argc < 2 || n < 1 || n > 10)
 		printf("Please input a number between 1 and 10!\n");
-	
+
 	else
-		printf("Input is: %d\n", n);
-	
+		printf("Input is: %d\n", n); //test input
+
+		//loop for the number of processes specified by user
 		for(int i = 1; i <= n; i++)
 		{
 			//while fork values return 0, child processes are running
@@ -33,13 +29,14 @@ int main(int argc, char* argv[])
 			{
 				//show child & parent process information
 				printf("Child %d running... Child PID: %d, Parent PID: %d.\n", i, getpid(), getppid());
-				
+
+				//run program b (simple print statement & exit)
 				char* argb[] = {"b_run", 0};
-				printf("Argument is: %s\n", argb[0]); 
+				printf("Argument is: %s\n", argb[0]);
 				execv(argb[0], &argb[0]);
 				//printf("Argument is: %s", argv[0]);
 				printf("Failed to execute program '%s'\n", argb[0]);
-				
+
 				//exit
 				exit(0);
 			}
@@ -47,17 +44,10 @@ int main(int argc, char* argv[])
 			{
 				//parent process waiting on child to terminate
 				wait(NULL);
+				//show that the parent is the same for all processes
 				printf("Hello from parent: %d!\n", getpid());
 			}
 		}
 
 	//return 0;
 }
-
-
-
-//Part 2: Write a program that takes a single int (n) from the command line and create a set of n processes. Each process should display a phrase "I am process x", where x is the process number within n, and then terminates.
-//The original parent process should not terminate until all children have terminated 
-
-
-
